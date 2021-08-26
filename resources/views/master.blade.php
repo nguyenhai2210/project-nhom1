8bc810bc7d3370a31dffdd78dad59cfb04ad9407
nguyenhai2210 <76729192+nguyenhai2210@users.noreply.github.com>
2021-08-26 17:19:19 +0700

    k có gì

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -33,7 +33,7 @@
             <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="">
                 <div class="sidebar-brand-icon ">
-                    <img width="225px" height="100px" src="https://www.bkacad.com/images/config/z2100070157967_78b4507a4b4b309b4dc866a23b6ed38c_1601355188.jpg" >
+                    <img width="225px" height="70px" src="https://www.bkacad.com/images/config/z2100070157967_78b4507a4b4b309b4dc866a23b6ed38c_1601355188.jpg" >
                 </div>
             </a>
 
